49e733bc9721f1fd790f4b35a346d0cb663a42d4
Ariel <Ariel>
2016-09-25 04:59:08 -0300

    1.3 se arregla web.js

--- web.js
+++ web.js
@@ -2,12 +2,19 @@ var express = require('express');
 
 var app= express.createServer(express.logger());
 
-app.get('/', function (request, response) {
+var path = require('path');
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+/*app.get('/', function (request, response) {
     
    // response.send('Hola mun3');
    // response.sendFile('home.html');
     
-});
+});*/
 var port = process.env.PORT || 5000;
 app.listen(port, function(){
     console.log("Listening on: " + port);
