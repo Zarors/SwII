4e70cbd2a3efde9f4f7a70678fcfbb5288818745
Ariel <Ariel>
2016-09-25 13:50:16 -0300

    1.3 se arregla web.js

--- web.js
+++ web.js
@@ -1,14 +1,37 @@
 var express = require('express');
 
-var app= express.createServer(express.logger());
+var app= express();
 
 var path = require('path');
 
 app.get('/',function(req,res){
-  res.sendFile('home.html');
+  res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
-
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/contact.html'));
+  //__dirname : It will resolve to your project folder.
+});
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/howtoplay.html'));
+  //__dirname : It will resolve to your project folder.
+});
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/sidebar-left.html'));
+  //__dirname : It will resolve to your project folder.
+});
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/sidebar-right.html'));
+  //__dirname : It will resolve to your project folder.
+});
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/signin.html'));
+  //__dirname : It will resolve to your project folder.
+});
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/signup.html'));
+  //__dirname : It will resolve to your project folder.
+});
 /*app.get('/', function (request, response) {
     
    // response.send('Hola mun3');
