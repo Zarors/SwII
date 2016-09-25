1185df3babaf5ad89e806cfe7a1bec9ca8b00129
Ariel <Ariel>
2016-09-25 04:27:16 -0300

    1.3 se arregla web.js

--- web.js
+++ web.js
@@ -1,14 +1,24 @@
 var express = require('express');
+var fs = require('fs');
 
-var app= express.createServer(express.logger());
+var app = express.createServer(express.logger());
 
-app.get('/', function (request, response) {
+fs.readfile/('./home.html', function(err,html){
+    if (err){
+        throw.err;
+    }
+    app.get('/', function (request, response) {
     
-    response.send('Hola mun3');
+   // response.send('Hola mun3');
    // response.open('index.html');
-    
+    response.writeHeader(200, {"Content-Type": "text/html"});  
+    response.write(html);  
+    response.end();  
 });
-window.location.href= 'home.html';
+}  );
+
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function(){
     console.log("Listening on: " + port);
